b7e9946f82a962f948fefe1c0f38018666d0121e
Heuller Lemes <heuller.lemes@outlook.com>
2022-11-09 23:54:33 -0300

    fix:books not handling empty thumbnail

--- src/Book/Books.js
+++ src/Book/Books.js
@@ -1,9 +1,34 @@
 import './Book.css';
 
 function Books({book, onMoveBook}) {
+  const options = [
+    {
+      name: 'Currently Reading',
+      value: 'currentlyReading'
+    },
+    {
+      name: 'Want To Read',
+      value: 'wantToRead'
+    },
+    {
+      name: 'Read',
+      value: 'read'
+    },
+    {
+      name: 'None',
+      value: 'none'
+    }
+  ]
     const onSelectChange = (event) => {
         onMoveBook(event.target.value)
     };
+    const preventPropagation = (event) => {
+      event.stopPropagation();
+    }
+
+    const thumbnail = () => {
+      return book.imageLinks?.thumbnail ? 'url('+book.imageLinks?.thumbnail+')' : 'none'
+    }
     return (
         <div className="book">
             <div
@@ -11,23 +36,25 @@ function Books({book, onMoveBook}) {
               style={{
                 width: 128,
                 height: 193,
-                backgroundImage:
-                  "url("+book.imageLinks.thumbnail+")",
+                backgroundImage: thumbnail(),
               }}>
             </div>
             <div>
-              <select value={book.shelf} onChange={onSelectChange}>
+              <select value={book.shelf ? book.shelf : 'none'}
+                      onClick={preventPropagation}
+                      onChange={onSelectChange}>
                 <option value="none" disabled>
                   Move to...
                 </option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
+                {
+                  options.map((option) => (
+                    <option key={option.value} value={option.value}>{option.name}</option>
+                  ))
+                }
               </select>
             </div>
-            <p className="book-title">{book.title}</p>
-            <p>{book.authors[0]}</p>
+            <p className="book-title">{book.title ? book.title : ''}</p>
+            <p>{book.authors?.lenght > 0 ? book.authors[0] : ''}</p>
         </div>
     )
 }
